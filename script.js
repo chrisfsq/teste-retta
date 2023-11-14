71f74864aae08dc713af59da56ba1b95cedd6802
chrisfsq <christianfsq@gmail.com>
2023-11-14 20:38:52 -0300

    update script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function toRoman() {
 
 
     if (!decimal || decimal < 1 || decimal > 5000) {
-        return res.textContent = "Digite um número de 1 a 5000"
+       res.innerHTML = "Digite um número de 1 a 5000"
+       res.style.color = "red"
+       return
     }
 
     let result = ''
@@ -32,7 +34,8 @@ function toRoman() {
         }
     }
 
-    res.innerHTML = result
+    res.innerHTML = result;
+    res.style.color = "light-green";
 
 }
 
@@ -42,7 +45,9 @@ function toDecimal() {
     let result = 0;
 
     if (!roman) {
-        return res.textContent = "Digite um número romano de I até MMMMM";
+        res.textContent = "Digite um número romano de I até MMMMM";
+        res.style.color = "red";
+        return
     }
 
     numerosDecimais = {
@@ -71,6 +76,9 @@ function toDecimal() {
     if (roman.length > 0) {
         return res.textContent = "Número romano inválido";
     }
+    
 
-    res.innerHTML = result
+    res.innerHTML = result;
+    res.style.color = "ligh-green";
 }
+
