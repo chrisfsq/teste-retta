c3f0e48478fd798872efcfc114c1e2a6bbffe0ed
chrisfsq <christianfsq@gmail.com>
2023-11-13 22:15:54 -0300

    adicionado script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+function toRoman() {
+    let decimal = parseInt(document.getElementById('n1').value);
+    let res = document.getElementById('result');
+
+
+    if (!decimal || decimal < 1 || decimal > 5000) {
+        return res.textContent = "Digite um número de 1 a 5000"
+    }
+
+    let result = ''
+
+    let numerosRomanos = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    for (let numeros in numerosRomanos) {
+        while (decimal >= numerosRomanos[numeros]) {
+            result += numeros
+            decimal -= numerosRomanos[numeros]
+        }
+    }
+
+    res.innerHTML = result
+
+}
+
+function toDecimal() {
+    let roman = document.getElementById('n2').value.toUpperCase();
+    let res = document.getElementById('result');
+    let result = 0;
+
+    if (!roman) {
+        return res.textContent = "Digite um número romano de I até MMMMM";
+    }
+
+    numerosDecimais = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    }
+
+    for (let numeral in numerosDecimais) {
+        while (roman.startsWith(numeral)) {
+            result += numerosDecimais[numeral];
+            roman = roman.slice(numeral.length);
+        }
+    }
+
+    if (roman.length > 0) {
+        return res.textContent = "Número romano inválido";
+    }
+
+    res.innerHTML = result
+}
